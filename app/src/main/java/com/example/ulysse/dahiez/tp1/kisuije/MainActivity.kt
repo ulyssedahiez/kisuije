1f1e0d393ba755f1e53259b1c9980a4bdca99e91
Ulysse Dahiez <dahiez.ulysse@gmail.com>
2024-02-05 13:47:03 +0100

    first commit

---
+++ app/src/main/java/com/example/ulysse/dahiez/tp1/kisuije/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ulysse.dahiez.tp1.kisuije
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

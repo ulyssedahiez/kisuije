accbaec7402a3e0fa5c3a4676dd6cfd45b75f16a
Ulysse Dahiez <dahiez.ulysse@gmail.com>
2024-02-16 19:03:32 +0100

    (dev): button jouer ok

--- app/src/main/java/com/example/ulysse/dahiez/tp1/kisuije/MainActivity.kt
+++ app/src/main/java/com/example/ulysse/dahiez/tp1/kisuije/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.ulysse.dahiez.tp1.kisuije
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonPlay : Button = findViewById(R.id.buttonPlay)
+
+        buttonPlay.setOnClickListener{
+            val intent = Intent(this,numbersPlayers::class.java)
+            startActivity(intent)
+        }
+
     }
 }

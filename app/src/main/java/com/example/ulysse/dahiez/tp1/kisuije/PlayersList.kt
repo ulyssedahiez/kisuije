1b12a1b7f4288262196b8b69e07ec6a3ebc7231f
Ulysse Dahiez <dahiez.ulysse@gmail.com>
2024-02-24 16:09:15 +0100

    (dev): add some features

--- app/src/main/java/com/example/ulysse/dahiez/tp1/kisuije/PlayersList.kt
+++ app/src/main/java/com/example/ulysse/dahiez/tp1/kisuije/PlayersList.kt
@@ -1,6 +1,8 @@
 package com.example.ulysse.dahiez.tp1.kisuije
+
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,30 +16,34 @@ class PlayersList : AppCompatActivity() {
         val backButton = findViewById<Button>(R.id.button_back)
 
         // Liste de joueurs
-        val players = listOf("Joueur 1", "Joueur 2", "Joueur 3","Joueur 1", "Joueur 2", "Joueur 3","Joueur 1", "Joueur 2", "Joueur 3","Joueur 1", "Joueur 2", "Joueur 3","Joueur 1", "Joueur 2", "Joueur 3","Joueur 1", "Joueur 2", "Joueur 3","Joueur 1", "Joueur 2", "Joueur 3","Joueur 1", "Joueur 2", "Joueur 3","Joueur 1", "Joueur 2", "Joueur 3")
+        val players = listOf("Joueur 1", "Joueur 2", "Joueur 3")
 
         // Créer et ajouter des TextView pour chaque joueur
         for (player in players) {
+            val playerLayout = LinearLayout(this)
+            playerLayout.orientation = LinearLayout.HORIZONTAL
+
             val playerTextView = TextView(this)
             playerTextView.text = player
             playerTextView.setBackgroundResource(R.drawable.background_white_rounded_corners)
             playerTextView.setTextColor(resources.getColor(R.color.yellow))
             playerTextView.setPadding(16, 16, 16, 16)
 
-            // Définir les marges pour créer de l'espace entre les éléments
-            val layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
+            // ImageButton pour supprimer un joueur
+            val deleteButton = ImageButton(this)
+            deleteButton.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT
             )
-            layoutParams.setMargins(
-                resources.getDimensionPixelSize(R.dimen.item_margin_horizontal),
-                0,
-                resources.getDimensionPixelSize(R.dimen.item_margin_horizontal),
-                resources.getDimensionPixelSize(R.dimen.item_margin_vertical)
-            )
-            playerTextView.layoutParams = layoutParams
+            deleteButton.setImageResource(R.drawable.garbage)
+            deleteButton.contentDescription = "Supprimer"
 
-            containerLayout.addView(playerTextView)
+            // Ajouter les vues à playerLayout
+            playerLayout.addView(playerTextView)
+            playerLayout.addView(deleteButton)
+
+            // Ajouter playerLayout à scrollViewLinearLayout
+            containerLayout.addView(playerLayout)
         }
 
         // Gérer le clic sur le bouton de retour
